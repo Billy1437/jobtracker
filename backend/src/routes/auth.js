522a66383155fe11580305268e5b0616ba6d6715
Billy1437 <minphonethit456@gmail.com>
2025-06-11 16:02:49 +0700

    finish2

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express"
 import { createUser, googleLogin, loginUser } from "../controllers/authControllers.js"
 const router = express.Router()
 
-router.post("/login",loginUser)
+router.post("/",loginUser)
 
 router.post("/google-login",googleLogin)
 
